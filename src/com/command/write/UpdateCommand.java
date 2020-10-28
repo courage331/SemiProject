744c194fc9ae9978a0a33678cb0b41fbf8f2d4d3
hoin <hoin@DESKTOP-NJGVEI8>
2020-10-28 23:17:35 +0900

    호인 : command들

--- src/com/command/write/UpdateCommand.java
+++ src/com/command/write/UpdateCommand.java
@@ -6,33 +6,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.lec.beans.ReviewDAO;
-// 1028 4:19 이호인 
-public class WriteCommand implements Command{
+// 1028 23:00 이호인
+public class UpdateCommand implements Command {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
-		
 		int cnt = 0;
 		ReviewDAO dao = new ReviewDAO();
 		
-		// 입력받는 곳
-		// 여기가 write문이니까 나중에 cus_num 받아넣을곳도 알아보자
-		int star = Integer.parseInt(request.getParameter("star"));
+		//입력한 값을 받아오기
+		int num = Integer.parseInt(request.getParameter("num"));
 		String subject = request.getParameter("subject");
 		String content = request.getParameter("content");
-		
-		// 유효성 체크 : null 이거나, 빈 문자열이면 
-		if(star != 0 && subject != null && subject.trim().length() > 0 && star > 0) {
-			
+		int star = Integer.parseInt(request.getParameter("star"));
+		// num으로 가져온 후 제목 내용 별점 등을 수정
+		if(subject != null && subject.trim().length() > 0) {
 			try {
-				cnt = dao.insert(subject, content, star);
-			} catch (SQLException e) {
+				cnt = dao.update(num, subject, content, star);
+			} catch(SQLException e) {
 				e.printStackTrace();
 			}
-		}
+		} 
 		
 		request.setAttribute("result", cnt);
-		
+
 	}
 
 }
