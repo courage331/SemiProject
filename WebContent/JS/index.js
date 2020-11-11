6e481b2c4868cb9af682bb82c2a7403da32c3eda
jjh5324 <jjh5324@kakao.com>
2020-11-11 19:13:43 +0900

    정호 : 수정

--- WebContent/JS/index.js
+++ WebContent/JS/index.js
@@ -1,8 +1,8 @@
 $('.slick-container').slick({
   infinite: true, //양방향 무한 모션
-  speed:300, // 슬라이드 스피드
+  speed:100, // 슬라이드 스피드
   autoplay: true, //자동플레이 유무( false시 자동플레이 안됨 )
   arrows: true, /* 화살표 */
-  autoplaySpeed:3000, // 자동플레이 스피드
+  autoplaySpeed:5000, // 자동플레이 스피드
   slidesToShow: 1
 });
